067972be2ec7d94cd3887348242f1f7dfc826f4c
Ricardo Zapata <ricardozapatacruz@gmail.com>
2024-03-27 21:19:59 -0500

    Creating Stages for Game and Welcome files

--- src/main/java/com/example/juegoahorcado/view/GameStage.java
+++ src/main/java/com/example/juegoahorcado/view/GameStage.java
@@ -1,4 +1,30 @@
 package com.example.juegoahorcado.view;
 
-public class GameStage {
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class GameStage extends Stage {
+    public GameStage() throws IOException{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/juegoahorcado/game-view.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        setTitle("Juego Ahorcado");
+        setResizable(false);
+        setScene(scene);
+        show();
+    }
+    public static GameStage getInstance() throws IOException{
+        return GameStageHolder.INSTANCE = new GameStage();
+    }
+    public static void deleteInstance(){
+        GameStageHolder.INSTANCE.close();
+        GameStageHolder.INSTANCE = null;
+    }
+    private static class GameStageHolder{
+        private static GameStage INSTANCE;
+    }
 }
--- src/main/java/com/example/juegoahorcado/view/WelcomeStage.java
+++ src/main/java/com/example/juegoahorcado/view/WelcomeStage.java
@@ -1,4 +1,30 @@
 package com.example.juegoahorcado.view;
 
-public class WelcomeStage {
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class WelcomeStage extends Stage {
+    public WelcomeStage() throws IOException{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/juegoahorcado/welcome-view.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        setTitle("Juego Ahorcado");
+        setResizable(false);
+        setScene(scene);
+        show();
+    }
+    public static WelcomeStage getInstance() throws IOException{
+        return WelcomeStageHolder.INSTANCE = new WelcomeStage();
+    }
+    public static void deleteInstance(){
+        WelcomeStageHolder.INSTANCE.close();
+        WelcomeStageHolder.INSTANCE = null;
+    }
+    private static class WelcomeStageHolder{
+        private static WelcomeStage INSTANCE;
+    }
 }
